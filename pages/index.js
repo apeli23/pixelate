55f90d11f480b0d020bf5712bbeefec558dabd5d
apeli23 <apelibrian@gmail.com>
2022-09-28 15:32:14 +0300

    tst

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,78 @@
 import { useRef, useEffect, useState } from "react";
+import Image from 'next/image'
 import html2canvas from "html2canvas";
 import { ImagePixelated, ElementPixelated } from "react-pixelate"
+import { buildUrl } from 'cloudinary-build-url';
 
 export default function Home() {
   const inputRef = useRef(undefined);
   const canvasRef = useRef(undefined);
   const imageRef = useRef(undefined);
+  const pximgRef = useRef(undefined);
+
+
 
   const [picture, setPicture] = useState(undefined);
   const [link, setLink] = useState('');
+  const [pximg, setPximg] = useState(undefined);
 
   let canvas, image, context, pixelArr, sample_size;
 
+  useEffect(() => {
+    if (picture) {
+      setTimeout(() => {
+        setPicture(picture);
+      }, 2000);
+    }
+  }, [picture])
   const changeHandler = (e) => {
     const file = e.target.files?.item(0);
     setPicture(URL.createObjectURL(file));
   }
 
+
+
   const uploadHandler = async () => {
     html2canvas(imageRef.current).then((canvas) => {
+      console.log(canvas.toDataURL());
       try {
         fetch('/api/upload', {
           method: 'POST',
           body: JSON.stringify({ data: canvas.toDataURL() }),
           headers: { 'Content-Type': 'application/json' },
         })
-          .then((response) => response.json())
+          .then((response) => response.json()) 
           .then((data) => {
+            console.log(data.data)
             setLink(data.data);
+          }).then(() => {
+            pixelate();
           });
       } catch (error) {
         console.error(error);
       }
     });
   };
+    const pixelate = () => {
+    console.log('pix');
+    const urlBlurred = buildUrl(link, {
+      cloud: {
+        cloudName: 'dogjmmett',
+      },
+      transformations: {
+        effect: "blur:10",
+        quality: 1
+      }
+    });
+    setPximg(urlBlurred);
+  }
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='column'>
-          {picture ?
-            <h3>original  image</h3>
-            :
-            <h3>select an image file</h3>
-          }
-          <input ref={inputRef} type="file" hidden onChange={changeHandler} />
+      <div className="row">
+        <div className="column">
+        <input ref={inputRef} type="file" hidden onChange={changeHandler} />
           <button onClick={() => { inputRef.current.click() }}>Select File</button><br />
-          {picture &&
-            <img src={picture} />
-          }
         </div>
-        {picture &&
-          <div className='column'>
-            <h2>Pixelated image</h2>
-            <div ref={imageRef} >
-              <ImagePixelated src={picture} width={500} height={300} fillTransparencyColor={"grey"} />
-            </div>
-            {link && <a href={link}><p>Get link</p></a>}
-            <button onClick={uploadHandler}>Upload Image</button>
-          </div>
-        }
       </div>
     </div>
   )
